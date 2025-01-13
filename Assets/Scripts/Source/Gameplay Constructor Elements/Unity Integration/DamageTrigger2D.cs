056faf948eb60672d0ed86255ed5bbad6dfbe9e2
francheskodown <frollite2@gmail.com>
2025-01-13 16:22:19 +0300

    Minor code Refactoring

--- Assets/Scripts/Source/Gameplay Constructor Elements/Unity Integration/DamageTrigger2D.cs
+++ Assets/Scripts/Source/Gameplay Constructor Elements/Unity Integration/DamageTrigger2D.cs
@@ -1,6 +1,6 @@
 using System;
 using GameplayConstructorElements.EntityExtensions;
-using GameplayConstructorFramework.Entity;
+using GameplayConstructorFramework.Entity.Unity;
 using GameplayConstructorFrameworkAPIs;
 using UnityEngine;
 
@@ -11,12 +11,12 @@ namespace GameplayConstructorElements.UnityIntegration
     {
         private void OnTriggerEnter2D(Collider2D otherEntityGameObject)
         {
-            if(!gameObject.TryGetComponent<IEntity>(out var entity) || !entity.TryGetDamageData(out var damage)) return;
-            if (!otherEntityGameObject.TryGetComponent<IEntity>(out var otherEntity)) return;
+            if (!gameObject.TryGetEntity(out var entity) || !entity.TryGetDamageData(out var damage)) return;
+            if (!otherEntityGameObject.TryGetEntity(out var otherEntity)) return;
             
             otherEntity.TakeDamage(damage);
         }
-
+        
         private void OnTriggerStay2D(Collider2D other)
         {
         }
